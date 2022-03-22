4ebc01505c4e9f156999bd59a2edc1c649aca013
Gurbir <devgurbir@gmail.com>
2022-03-22 15:45:25 +0530

    added file upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,6 +9,6 @@ const upload = require("../utils/fileUpload");
 
 router.get("/", getAllUsers);
 router.get("/create", showCreatePage);
-router.post("/create", createUser);
+router.post("/create", upload.single("avatar"), createUser);
 
 module.exports = router;
